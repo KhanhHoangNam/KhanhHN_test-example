687c6232b28f5cf31647fd9889dd6743c1411991
Khanh Hoang Nam <chienbinhbattu9xpro@gmail.com>
2018-11-15 22:40:15 +0700

    create a new employee

--- src/main/java/com/codegym/example/controller/EmployeeController.java
+++ src/main/java/com/codegym/example/controller/EmployeeController.java
@@ -1,14 +1,31 @@
 package com.codegym.example.controller;
 
+import com.codegym.example.model.Employee;
+import com.codegym.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EmployeeController {
-    @GetMapping("/")
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/create-employee")
     public ModelAndView showCreateForm() {
         ModelAndView modelAndView = new ModelAndView("views/create");
+        modelAndView.addObject("employee", new Employee());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-employee")
+    public ModelAndView saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        ModelAndView modelAndView = new ModelAndView("views/create");
+        modelAndView.addObject("employee", employee);
         return modelAndView;
     }
 }
